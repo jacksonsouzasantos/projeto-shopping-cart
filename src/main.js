c5f9fe7a4ca9269fb2b2b0d5b2c5bfbf3cc5612b
Jackson Souza Santos <jacksonsantos.tec@gmail.com>
2022-11-17 17:56:46 -0300

    requisito 3

--- src/main.js
+++ src/main.js
@@ -2,3 +2,16 @@ import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const listProductSection = document.querySelector('.products');
+
+const showProducts = (products) => {
+  products.forEch((product) => {
+    const liProduct = createCustomElement(product);
+    listProductSection.appendChild(liProduct);
+  });
+};
+
+window.onload = () => {
+  showProducts();
+};
