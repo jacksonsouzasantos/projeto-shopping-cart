0a0cca3d7ff31e2941b262ea362ab4eb8aa60838
Jackson Souza Santos <jacksonsantos.tec@gmail.com>
2022-11-18 16:48:25 -0300

    resolvendo requisito 3 e 4

--- src/main.js
+++ src/main.js
@@ -1,17 +1,46 @@
 import { searchCep } from './helpers/cepFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-const listProductSection = document.querySelector('.products');
+const productsList = document.querySelector('.products');
 
-const showProducts = (products) => {
-  products.forEch((product) => {
-    const liProduct = createCustomElement(product);
-    listProductSection.appendChild(liProduct);
-  });
+const startLogin = (elemento) => {
+  const runLogin = document.createElement('runLogin');
+  runLogin.innerHTML = 'carregando...';
+  runLogin.className = 'loading';
+  elemento.appendChild(runLogin);
+};
+
+const endLogin = (elemento) => {
+  const erroLogin = document.createElement('erroLogin');
+  erroLogin.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  erroLogin.className = 'error';
+  elemento.appendChild(erroLogin);
+};
+
+const injectProducts = async (elemento) => {
+  try {
+    const data = await fetchProductsList('computador');
+    data.forEach((product) => {
+      const element = createProductElement(product);
+      elemento.appendChild(element);
+      console.log(injectProducts());
+    });
+  } catch (error) {
+    endLogin(elemento);
+  }
+};
+
+const removeLog = () => {
+  const p = document.querySelector('.loading');
+  p.remove();
 };
 
 window.onload = () => {
-  showProducts();
+  startLogin(productsList);
+  injectProducts(productsList);
+  removeLog();
 };
